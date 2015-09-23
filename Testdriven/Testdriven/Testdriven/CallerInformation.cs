fd3a848149323d68c0a227194ec63075e94ad4da
stefer <stefan.piaostefan@gmail.com>
2015-09-23 23:25:34 +0200

    Added som shorts tests of CallerInformation attributes

---
+++ Testdriven/Testdriven/Testdriven/CallerInformation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testdriven
+{
+    public class CallerInformation
+    {
+        public string GetCallerMemberName([CallerMemberName] string caller = null)
+        {
+            return caller;
+        }
+
+        public string GetCallerFillePath([CallerFilePath] string path = null)
+        {
+            return path;
+        }
+
+        public int GetCallerLineNumber([CallerLineNumber] int line = -1)
+        {
+            return line;
+        }
+    }
+}
---
+++ Testdriven/Testdriven/Tests/CallerInformationTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testdriven;
+
+namespace Tests
+{
+    [TestClass]
+    public class CallerInformationTests
+    {
+
+        CallerInformation ci = new CallerInformation();
+
+        [TestMethod]
+        public void CallerMemberName_ShouldReturnCallingMethodName()
+        {
+            Assert.AreEqual(ci.GetCallerMemberName(), "CallerMemberName_ShouldReturnCallingMethodName");
+        }
+
+        [TestMethod]
+        public void CallerFilePath_ShouldReturnPathToCallingMethodSourceFile()
+        {
+            Assert.IsTrue(ci.GetCallerFillePath().Contains("CallerInformationTests.cs"));
+        }
+
+        [TestMethod]
+        public void CallerLineNumber_ShouldReturnCallingMethodLineNumber()
+        {
+            int line = ci.GetCallerLineNumber();
+            Assert.IsTrue(line > 31 && line < 35);
+        }
+    }
+}
